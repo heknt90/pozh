40d5b96be2b6d514ba9954ba3079c494fc3ca362
Nikita Efremov <heknt90@mail.ru>
2023-11-25 12:43:59 +0300

    moved and updated manifest.json and browserconfig.xml to root directory install button for pwa added update service worker registration

--- src/js/main.js
+++ src/js/main.js
@@ -176,11 +176,37 @@ function updateShortAnswers(dataTicket) {
 }
 
 if ('serviceWorker' in navigator) {
-  window.addEventListener('load', () => {
-    navigator.serviceWorker.register('/service-worker.js').then(registration => {
-      console.log("SW registered: ", registration)
-    }).catch(registrationError => {
-      console.log("SW registration failed: ", registrationError)
-    })
-  })
+  navigator.serviceWorker
+    .register('/service-worker.js')
+    .then(() => { console.log('Service Worker Registered'); });
 }
+
+// Code to handle install prompt on desktop
+
+let deferredPrompt;
+const addBtn = buttonInstall;
+
+window.addEventListener('beforeinstallprompt', (e) => {
+  // Prevent Chrome 67 and earlier from automatically showing the prompt
+  e.preventDefault();
+  // Stash the event so it can be triggered later.
+  deferredPrompt = e;
+  // Update UI to notify the user they can add to home screen
+  addBtn.style.display = 'block';
+
+  addBtn.addEventListener('click', () => {
+    // hide our user interface that shows our Firemans button
+    addBtn.style.display = 'none';
+    // Show the prompt
+    deferredPrompt.prompt();
+    // Wait for the user to respond to the prompt
+    deferredPrompt.userChoice.then((choiceResult) => {
+      if (choiceResult.outcome === 'accepted') {
+        console.log('User accepted the Firemans prompt');
+      } else {
+        console.log('User dismissed the Firemans prompt');
+      }
+      deferredPrompt = null;
+    });
+  });
+});
